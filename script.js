f95e071511344b63121556e86cb27d794f6f2f31
Arkadiusz Kulewicz <a.kulewicz@gmail.com>
2021-04-24 17:17:12 +0200

    Add basic html structur and fetch data from Wordpress API

---
+++ script.js
@@ -0,0 +1,41 @@
+const postsBox = document.querySelector('.posts--js');
+
+const getPost = async (id) => {
+    const url = 'https://www.poruszeni.pl/wp-json/wp/v2/posts';
+    const response = await fetch(url);
+    const data = await response.json();
+    return data[id];
+}
+
+const getPostImage = async (id) => {
+    const url = `https://poruszeni.pl/wp-json/wp/v2/media/${id}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data;
+}
+
+const getPostData = async (id) => {
+    const postData = await getPost(id)
+    const postImage = await getPostImage(postData.featured_media)
+    return {postData, postImage}
+}
+
+const updateUI = (data) => {
+    
+    const image = data.postImage;
+    const content = data.postData;
+    let article = `
+    <article class="post">
+        <img class="post__image" src="${image.source_url}">
+        <p class="post__info">Kategoria | Mar 18, 2020</p>
+        <h3 class="post__title">${content.title.rendered}</h3>
+        <p class="post__excerpt">${content.excerpt.rendered}</p>
+    </article>
+    `
+   postsBox.innerHTML += article;
+}
+
+for(let i=0; i<=9; i++) { 
+    getPostData(i)
+        .then(data => updateUI(data))
+}
