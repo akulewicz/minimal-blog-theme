bb939b354d8f3c6699580c24b07b02d4f6bcf64e
Arkadiusz Kulewicz <a.kulewicz@gmail.com>
2021-05-12 21:02:36 +0200

    Add a js script that opens and closes the search form.

--- functions.php
+++ functions.php
@@ -6,7 +6,8 @@ function minimal_files() {
     wp_enqueue_style('google-fonts', 'https://fonts.googleapis.com/css2?family=Inter:wght@300;500&family=Lato:wght@300;400;700&family=Roboto+Slab:wght@300;400;500;700&display=swap');
     wp_enqueue_style('font-awsome', 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css');
     wp_enqueue_style('minimal_main_styles', get_stylesheet_uri());
-    wp_enqueue_script('main-js', get_theme_file_uri('/js/script.js'), NULL, '1.0', true);
+    wp_enqueue_script('hamburger-menu', get_theme_file_uri('/js/mobile.js'), NULL, '1.0', true);
+    wp_enqueue_script('search-box', get_theme_file_uri('/js/search.js'), NULL, '1.0', true);
 }
 
 add_action('wp_enqueue_scripts', 'minimal_files');
--- header.php
+++ header.php
@@ -1,31 +1,33 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
-  <head>
-    <meta charset="<?php bloginfo('charset'); ?>" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Poruszeni.pl</title>
-    <?php wp_head(); ?>
-  </head>
-  <body>
-    <div class="search-box">
-    <button class="search-box__close"><i class="fas fa-times"></i></button>
+
+<head>
+  <meta charset="<?php bloginfo('charset'); ?>" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Poruszeni.pl</title>
+  <?php wp_head(); ?>
+</head>
+
+<body>
+  <div class="search-box search-box--js">
+    <button class="search-box__close search-box__close--js"><i class="fas fa-times"></i></button>
     <form class="search-form" action="/" method="get">
-    <input class="search-form__input" type="text" name="s" id="search" placeholder="Wpisz szukaną frazę" />
-    <button class="search-form__button"><i class="fas fa-search search-form__icon"></i></button>
-</form>
-    </div>
-    <header class="header">
-      <button class="hamburger hamburger--js"><i class="fas fa-bars"></i></button>
-      <button class="search-button"><i class="fas fa-search"></i></button>
-      <nav class="top-navigation top-navigation--js">
-        
-        <?php wp_nav_menu(array(
-          'theme_location' => 'header-menu',
-          'container' => 'ul',
-          'menu_class' => 'top-navigation__list'
-          )); ?>
-          <!-- <li class="top-navigation__item">
+      <input class="search-form__input" type="text" name="s" id="search" placeholder="Wpisz szukaną frazę" />
+      <button class="search-form__button"><i class="fas fa-search search-form__icon"></i></button>
+    </form>
+  </div>
+  <header class="header">
+    <button class="hamburger hamburger--js"><i class="fas fa-bars"></i></button>
+    <button class="search-button search-button--js"><i class="fas fa-search"></i></button>
+    <nav class="top-navigation top-navigation--js">
+
+      <?php wp_nav_menu(array(
+        'theme_location' => 'header-menu',
+        'container' => 'ul',
+        'menu_class' => 'top-navigation__list'
+      )); ?>
+      <!-- <li class="top-navigation__item">
             <a class="top-navigation__link" href="#">Strona główna</a>
           </li>
           <li class="top-navigation__item">
@@ -37,8 +39,8 @@
           <li class="top-navigation__item">
             <a class="top-navigation__link" href="#">Kontakt</a>
           </li> -->
-         
-        <!-- </ul>    -->
-      </nav>
-      <h1 class="header__title"><a class="header__home-link" href="<?php echo site_url('');?>"><?php bloginfo('name'); ?></a></h1>
-    </header>
+
+      <!-- </ul>    -->
+    </nav>
+    <h1 class="header__title"><a class="header__home-link" href="<?php echo site_url(''); ?>"><?php bloginfo('name'); ?></a></h1>
+  </header>
